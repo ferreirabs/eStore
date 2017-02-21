8f28705c70657dbd6784eb71db3cc15baa0b1f40
jeffersonandrade <jeff1411>
2017-02-21 11:03:09 -0300

    Commit Classes Pedido

---
+++ eStore.Entities/Pedido.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace eStore.Entities
+{
+    public class Pedido
+    {
+        [RegularExpression(@"^\d+$", ErrorMessage = "O id deve ser numérico")]
+        [Key]
+        public int id { get; set; }
+
+        [Required(ErrorMessage = "O subtotal do pedido é obrigatório")]
+        public decimal subtotal { get; set; }
+
+        [Required(ErrorMessage = "O total do pedido é obrigatório")]
+        public decimal total { get; set; }
+
+        [Required(ErrorMessage = "A data do pedido é obrigatória")]
+        public DateTime data_pedido { get; set; }
+
+        public int status { get; set; }
+
+        public DateTime data_alteracao { get; set; }
+
+        //[Required(ErrorMessage = "O código do comprador é obrigatório")]
+        //public Comprador comprador { get; set; }
+
+        //[Required(ErrorMessage = "O código do método de pagamento é obrigatório")]
+        //public MetodoPagamento metodo_pagamento { get; set; }
+
+        //[Required(ErrorMessage = "O código do transportadora é obrigatório")]
+        //public Transportadora transportadora { get; set; }
+    }
+}
---
+++ eStore.Entities/PedidoDAO.cs
@@ -0,0 +1,23 @@
+﻿using System.Data;
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.Data.SqlTypes;
+using System.Data.Linq.SqlClient;
+using System.Text;
+using System.Data.Entity;
+using eStore.Entities.Context;
+
+namespace eStore.Entities
+{
+    public class PedidoDAO : IPedidoDAO
+    {
+        eStoreContext db = new eStoreContext();
+
+        public int QuantidadePedidos()
+        {
+            int total = 0;
+            //total = db.Pedido.FirstOrDefault(p => p.id.Equals(1)).id;
+            return total;
+        }
+    }
+}
