3c8f05b807bdebed074f539a6518a53fccf3f8fd
Bruno Ferreira <ferreira.bs@gmail.com>
2017-02-21 00:21:22 -0300

    Adicionada classe de Frete no projeto Entitie

---
+++ eStore.Entities/Frete.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace eStore.Entities
+{
+    [Serializable]
+    public class Frete
+    {
+        [RegularExpression(@"^\d+$", ErrorMessage = "O id deve ser numérico")]
+        [Key]
+        public int id { get; set; }
+
+        [Required(ErrorMessage = "O código do frete é obrigatório")]
+        public string codigo { get; set; }
+
+    }
+}
