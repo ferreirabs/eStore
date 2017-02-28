7848440f93c8ac0a3547e664f6d48fc56049b91f
Bruno Ferreira <ferreira.bs@gmail.com>
2017-02-28 11:28:28 -0300

    Ajuste Classes Comprador Faltando

---
+++ eStore.Entities/Comprador.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace eStore.Entities
+{
+    [Serializable]
+    public class Comprador
+    {
+        [Key]
+        public int id { get; private set; }
+
+        public string codigo { get; set; }
+        public string nome { get; set; }
+    }
+}
