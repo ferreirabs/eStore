81f37c1b7dd9f1d77b3799bb35ad4591904f54be
Bruno Ferreira <ferreira.bs@gmail.com>
2017-02-28 11:33:23 -0300

    Ajuste - Add ModelCategoria.cs

---
+++ eStore/Model/ModelCategoria.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Data.Entity.Infrastructure;
+using eStore.Entities;
+
+namespace eStore.Model
+{
+    [Serializable]
+    public class ModelCategoria : Categoria
+    {
+        public int id { get; set; }
+
+        public ModelCategoria() { }
+
+        public ModelCategoria(Categoria categoria)
+        {
+
+            this.id = categoria.id;
+            this.codigo = categoria.codigo;
+            this.nome = categoria.nome;
+
+        }
+    }
+
+}
